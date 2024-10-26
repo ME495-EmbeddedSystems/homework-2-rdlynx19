01b7e162a64e78cd659d057428b2fb8fc92984ff
rdlynx19 <pushkardave.vnit@gmail.com>
2024-10-26 01:27:03 -0500

    Resolved flake8 errors for physics module

--- turtle_brick/turtle_brick/physics.py
+++ turtle_brick/turtle_brick/physics.py
@@ -1,4 +1,6 @@
+"""Physics Module for the Brick."""
 import math
+
 
 class World:
     """Keep track of the physics of the world."""
@@ -17,7 +19,6 @@ class World:
         self.gravity = gravity
         self.platform_radius = radius
         self.time_step = dt
-        
 
     @property
     def brick(self):
@@ -42,21 +43,19 @@ class World:
         self._brick[2] = location.z
 
     def drop(self):
-        """
-        Update the brick's location by having it fall in gravity for one timestep
-        """
-        self._brick[2] = self._brick[2] - (self.gravity*self.time_step)*self.time_step
+        """Update the brick's location using gravity for one timestep."""
+        self._brick[2] = self._brick[2] - (self.gravity*self.time_step)*(
+            self.time_step)
         # pass
 
     def drop_brick_z(self, tilt_angle):
-        """
-        Update the brick's location by falling from the platform
-        """
-        # self._brick[0] = self._brick[0] - (self.gravity*math.sin(tilt_angle)*self.time_step)*self.time_step
-        self._brick[2] = self._brick[2] - (self.gravity*math.cos(tilt_angle)*self.time_step)*self.time_step
+        """Update the brick's location by falling from the platform."""
+        self._brick[0] = self._brick[0] - (self.gravity*(
+            math.sin(tilt_angle))*self.time_step)*self.time_step
+        self._brick[2] = self._brick[2] - (self.gravity*(
+            math.cos(tilt_angle))*self.time_step)*self.time_step
 
     def drop_brick_x(self, tilt_angle):
-        """
-        Update the brick's x location when falling from the platform
-        """
-        self._brick[0] = self._brick[0] - (self.gravity*math.sin(tilt_angle)*self.time_step)*self.time_step
+        """Update the brick's x location when falling from the platform."""
+        self._brick[0] = self._brick[0] - (self.gravity*(
+            math.sin(tilt_angle))*self.time_step)*(self.time_step)
